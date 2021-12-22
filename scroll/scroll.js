34dd8f6ac8aa5e651136b2dfbef686371c0c26f7
lodenrogue <phyzix5761@gmail.com>
2021-12-22 15:25:53 -0500

    Added scrolling up and down half page

--- scroll/scroll.js
+++ scroll/scroll.js
@@ -3,6 +3,8 @@ class Scroll {
     registerKeys() {
         registry.register(["g", "g"], () => this.scrollToTop());
         registry.register(["G"], () => this.scrollToBottom());
+        registry.register(["d"], () => this.scrollDown());
+        registry.register(["u"], () => this.scrollUp());
     }
 
     scrollToTop() {
@@ -11,6 +13,14 @@ class Scroll {
 
     scrollToBottom() {
         scroll(0, document.body.scrollHeight)
+    }
+
+    scrollDown() {
+        scrollBy(0, window.innerHeight / 2);
+    }
+
+    scrollUp() {
+        scrollBy(0, -(window.innerHeight / 2));
     }
 }
 
